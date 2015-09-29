f2bb6319575fc0016935ce98b18ad45fbcda9604
Catherine <ducharme.catherine@gmail.com>
2015-09-29 20:49:08 +0000

    Built skeleton of a web server that seraches through an object based on a query from the user. Not working yet!

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
+
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var entries = {
+  1: {
+    firstName: "John",
+    lastName: "Smith",
+    emails: [
+      {type: "home", address: "john@smith.com"},
+      {type: "work", address: "jsmith@megacorp.com"}
+    ]
+  },
+  2: {
+    firstName: "Gilbert",
+    lastName: "Robert",
+    phoneNumber: [
+      {type: "home", phoneNumber: "514-123-4567"},
+      {type: "other", phoneNumber: "819-659-6383"}
+      ]
+  },
+  3: {
+    firstName: "Giselle",
+    lastName: "Labelle",
+    emails: [
+      {type: "home", address: "giselle@gmail.com"},
+      {type:"work", address: "giselle@decodemtl.com"}
+      ]
+  },
+  4: {
+    firstName: "Bright",
+    lastName: "Sparks",
+    phoneNumber: [
+      {type:"home", phoneNumber: "416-987-5678"},
+      {type:"work", phoneNumber: "740-237-7522"}
+      ]
+  }
+};
+
+
+app.get('/entry/search', function (req, res) {
+  var queries = {};
+  if (req.query.firstName != null) {
+    queries.firstName = req.query.firstName;
+  }
+  if (req.query.lastName != null) {
+    queries.lastName = req.query.lastName;
+  }
+  if (req.query.email != null) {
+    queries.email = req.query.email;
+  }
+  if (req.query.phoneNumber != null) {
+    queries.phoneNumber = req.query.phoneNumber;
+  }
+  //res.send(queries);
+  
+  var keys = Object.keys(queries);
+  //res.send(keys);
+  
+  var entriesKeys = Object.keys(entries);
+  //res.send(entriesKeys)
+  
+  for (var keys in queries) {
+    for (var entry in entries) {
+      if (entry.hasOwnProperty(keys)) {
+        res.send(entry);
+      }
+    }
+  } 
+
+
+/*  
+  for (var key in entries) {
+    if (entries.hasOwnProperty(key)) {
+        console.log(key);
+    }
+}
+  if (req.query.id != null) {
+    res.send('id: ' + req.query.id);
+  }
+  else {
+    res.sendStatus(404);
+  }*/
 });
-
-
-
-
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
