00c86ddd4e22b26cfd3f4ca75fc579a4a0b3025f
Catherine <ducharme.catherine@gmail.com>
2015-09-29 19:19:23 +0000

    Created a web server that does an operation on two inputted numbers

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/op/:operation/:number1/:number2', function (req, res) {
+  var operation = (req.params.operation).toLowerCase();
+  var num1 = Number(req.params.number1);
+  var num2 = Number(req.params.number2);
+  var solution = 0;
+  function operator(operation, num1, num2) {
+    switch(operation) {
+      case "add":
+        solution = num1 + num2;
+        break;
+      case "sub":
+        solution = num1 - num2;
+        break;
+      case "mult":
+        solution = num1 * num2;
+        break;
+      case "div":
+        solution = num1 / num2;
+        break;
+      default:
+        solution = null;
+    }
+  }
+  operator(operation, num1, num2);
+  if (solution != null) {
+    res.json({"operator": operation,
+    "firstOperand": num1,
+    "secondOperand": num2,
+    "solution": solution
+  });
+  }
+  else {
+    res.sendStatus(404);
+  }
 });
-
-
-
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
